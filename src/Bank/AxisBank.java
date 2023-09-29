c0d6f4f2e8bebf7646db0034bdd3a79acd3bd14d
vidyaneeraj <vidyakdev@gmail.com>
2023-09-29 15:29:22 +0400

    Welcome

---
+++ src/Bank/AxisBank.java
@@ -0,0 +1,14 @@
+package Bank;
+
+public class AxisBank extends BankInfo {
+public void deposit (int fl) {
+	System.out.println("Total number of deposits in Axis bank is"+fl);
+}
+public static void main(String[] args) {
+	AxisBank ab =new AxisBank();
+	ab.saving(650);
+	ab.fixed();
+	ab.deposit(500);
+	
+}
+}
---
+++ src/Bank/BankInfo.java
@@ -0,0 +1,17 @@
+package Bank;
+
+public class BankInfo {
+	public void saving(int s) {
+		System.out.println("The number of savings bank holders"+s);
+		// TODO Auto-generated method stub
+	}
+	public void fixed() {
+		System.out.println("There are 256 fixed accounts");
+		// TODO Auto-generated method stub
+	}
+	public  void deposit(int f) {
+		// TODO Auto-generated method stub
+System.out.println("The total number of deposits is"+f);
+	}
+
+}
